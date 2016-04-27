6c85e6f1772a048c01a527ab07f31181e56236ca
aylincsknpnr <aylin_comu@hotmail.com>
2016-04-27 16:25:44 +0300

    Slide Menu Uygulama

---
+++ HeaderView/app/src/main/java/com/example/aylin/headerview/AttachmentFragment.java
@@ -0,0 +1,20 @@
+package com.example.aylin.headerview;
+
+/**
+ * Created by aylin on 19.04.2016.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AttachmentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.attachment_fragment, container, false);
+        return rootView;
+    }
+
+}
