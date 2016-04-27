4d1c870cbc9ab8398adfe3f5eda309a7e519cccb
aylincsknpnr <aylin_comu@hotmail.com>
2016-04-27 10:46:40 +0300

    gelincik giriş

---
+++ MenuLogin/app/src/main/java/com/example/aylin/menulogin/Caller.java
@@ -0,0 +1,32 @@
+package com.example.aylin.menulogin;
+import android.content.Context;
+import android.widget.Toast;
+
+
+/**
+ * Created by aylin on 20.04.2016.
+ */
+public class Caller extends Thread
+{
+    private static Context context;
+
+    public CallSoap cs;
+    public String a,b;
+public String resp;
+    public void run(){
+        try{
+            cs=new CallSoap();
+             resp=cs.Call(a, b);
+            Second_Fragment.rslt=resp;
+            Third_Fragment.rslt=resp;
+
+        }catch(Exception ex)
+        {Second_Fragment.rslt=ex.toString();}
+    }
+    public String ReturnValue(){
+       // System.out.println("Dönen Değer:" + Second_Fragment.rslt);
+        return Second_Fragment.rslt;
+    }
+
+
+}
