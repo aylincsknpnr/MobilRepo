9f80180b363e6286f1f73bc44dd54c0a5e936699
aylincsknpnr <aylin_comu@hotmail.com>
2016-05-06 10:37:51 +0300

    menu ilgili apk çalıştırılması çözüldü

---
+++ NavigationView-master/app/src/main/java/com/android4dev/navigationview/UserModel.java
@@ -0,0 +1,18 @@
+package com.android4dev.navigationview;
+
+/**
+ * Created by aylin on 05.05.2016.
+ */
+public class UserModel {
+    public int id;
+    public String username;
+    public String password;
+    public UserModel(int id, String username, String password) {
+// TODO Auto-generated constructor stub
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+    public UserModel() {
+    }
+}
